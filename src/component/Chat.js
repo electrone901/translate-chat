92d078229b3864ea2530f808fe9e089a8cd15fa1
jason <jasonedlewis@gmail.com>
2019-12-04 15:24:01 -0500

    Landing page look set up

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,14 +1,11 @@
   import React from 'react';
+  import '../CSS/Chat.css'
 
   const Chat = ({}) => {
-      return ( <
-          div className = "container" >
-          <
-          div className = "row text-center space-top" >
-          <
-          div className = "col-12 col-sm-12 col-md-12" >
-          <
-          h2 className = "title" > Confirm the information below < /h2> < /
+      return ( <div className = "container" >
+          <div className = "row text-center space-top" >
+          <div className = "col-12 col-sm-12 col-md-12" >
+          <h2 className = "title" > Welcome to Translate Chat! < /h2> < /
           div > <
           /div> < /
           div >
